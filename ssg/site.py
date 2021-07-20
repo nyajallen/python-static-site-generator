fe8c5a8cb4c98095895d77d92a8e0c77fed70b2c
Nya Allen <allen.nya24@gmail.com>
2021-07-20 15:26:27 -0400

    correct mistakes for section 2

--- ssg/site.py
+++ ssg/site.py
@@ -1,18 +1,18 @@
-import pathlib
+from pathlib import Path
 import os
 
 class Site():
-    class Site(self, source, dest):
+    def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest +'/'+ path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
-    def build():
+    def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
 
         for path in self.source.rglob("*"):
-            if (os.path.isdir(path)):
+            if path.isdir():
                 create_dir(path)
