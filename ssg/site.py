23212a24d55d16eb33dc69c1351611ce2d2f2cf1
Nya Allen <allen.nya24@gmail.com>
2021-07-20 15:27:49 -0400

    Check if path is a directory

--- ssg/site.py
+++ ssg/site.py
@@ -14,5 +14,5 @@ class Site():
         self.dest.mkdir(parents=True, exist_ok=True)
 
         for path in self.source.rglob("*"):
-            if path.isdir():
+            if path.is_dir():
                 create_dir(path)
