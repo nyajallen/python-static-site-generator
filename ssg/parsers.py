a10a9db91c1c27e1bc62fdb0a07cd6d9d6eb2355
Nya Allen <allen.nya24@gmail.com>
2021-07-20 16:57:23 -0400

    Update parsers.py

---
+++ ssg/parsers.py
@@ -0,0 +1,12 @@
+from typing import List
+from pathlib import Path
+
+class Parser():
+    extensions: List[str]
+
+    def valid_extension(self, extension):
+        return extension in self
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError()
+        
