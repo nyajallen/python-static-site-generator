2e83da8a079b06ad60982248f021a81a513ea9a3
Nya Allen <allen.nya24@gmail.com>
2021-07-27 16:15:10 -0400

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -4,15 +4,17 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
 
-    def load(self, cls, string):
-        (_, fm, content) = __regex.split(string, 2)
-        load(fm, Loader=FullLoader)
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
+        self.data["content"] = content
 
     @property
     def body(self):
@@ -20,7 +22,24 @@ class Content(Mapping):
 
     @property
     def type(self):
-        return
+        return self.data["type"] if "type" in self.data else None
 
+    @type.setter
+    def type(self):
+        self.data["type"] = type
 
-class __regex: re.compile(__delimeter, re.MULTILINE)
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for key, value in data.items():
+            if key != "content":
+                data[key] = value
+        return str(data)
