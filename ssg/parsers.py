2871eec827947987ca601f5d5c5351bc191b9f97
Nya Allen <allen.nya24@gmail.com>
2021-07-27 15:13:13 -0400

    finish site.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -17,13 +17,13 @@ class Parser:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
-        full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
 
         with open(full_path, 'w') as file:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(path, dest / path)
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):
